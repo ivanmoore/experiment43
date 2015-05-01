27a35ed21759e85dd25373a1307dcf4ee416bbe3
Ivan Moore <ivan@teamoptimization.com>
2015-05-01 16:19:35 +0100

    another attempt, with conditional

--- flow.groovy
+++ flow.groovy
@@ -4,7 +4,11 @@ node {
     node {
         git url: 'https://github.com/ivanmoore/experiment43.git'
         def ttt = thung()
-        echo "thing was ${ttt}"
+        if (true) {
+        	echo "thing was ${ttt}"
+        } else {
+                echo "did not like that"
+        }
     }
 
     stage 'deployish'
