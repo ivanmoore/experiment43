1746922a34a16ba96361152c8a0611d6767efcf8
Ivan Moore <ivan@teamoptimization.com>
2015-05-01 14:18:07 +0100

    try to do function

--- flow.groovy
+++ flow.groovy
@@ -1,7 +1,10 @@
-if ('a string'.length()>0) {
-    stage 'big thing'
-} else {
-    stage 'small thing'
+def unknown(){
+    if ('a string'.length()>0) {
+        stage 'big thing'
+    } else {
+        stage 'small thing'
+    }
 }
+
 stage 'buildingish'
 stage 'deployish'
