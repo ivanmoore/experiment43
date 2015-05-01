105cc0d07a5eab042e9c33ab4985bf43917d64fa
Ivan Moore <ivan@teamoptimization.com>
2015-05-01 16:33:33 +0100

    conditional stages within flyweight

--- flow.groovy
+++ flow.groovy
@@ -1,16 +1,19 @@
+def ttt = thung()
+
+if (cond()) {
+    echo "thing was ${ttt}"
+    stage 'clever'
+} else {
+    echo "did not like that"
+    stage 'tricky'
+}
+
 stage 'configurish'
 node {
     stage 'buildingish' 
     node {
         git url: 'https://github.com/ivanmoore/experiment43.git'
-        def ttt = thung()
-        if (cond()) {
-        	echo "thing was ${ttt}"
-                stage 'clever'
-        } else {
-                echo "did not like that"
-                stage 'tricky'
-        }
+        echo 'something'
     }
 
     stage 'deployish'
